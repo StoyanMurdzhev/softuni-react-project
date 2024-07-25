dca8ea77fc0a42151cdf2eea6d7d07dd6a0589f1
StoyanMurdzhev <smourdzhev@gmail.com>
2024-07-25 17:40:44 +0300

    Update Login.jsx to allow login

--- vite-react-project/src/components/login/Login.jsx
+++ vite-react-project/src/components/login/Login.jsx
@@ -1,15 +1,59 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { login } from "../../services/userService";
+
+
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    const sumbitHandler = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            await login(email, password);
+            navigate("/");
+        } catch (error) {
+            if (error.code === "auth/wrong-password" || error.code === "auth/user-not-found") {
+                setError("Incorrect email or password.");
+            } else if (error.code === "auth/invalid-email") {
+                setError("Invalid email address.");
+            } else {
+                setError("Error logging in.");
+            }
+            console.log(error);
+            setPassword("");
+        }
+    }
+
+
     return (
         <>
-            <form>
+            <form onSubmit={sumbitHandler}>
                 <label htmlFor="email">
                     Email:
-                    <input type="email" name="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </label>
                 <label htmlFor="password">
                     Password:
-                    <input type="password" name="password" />
+                    <input type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required />
                 </label>
+                {error && <p style={{color: "red"}}>{error}</p>}
                 <button type="submit">Log in</button>
             </form>
         </>
