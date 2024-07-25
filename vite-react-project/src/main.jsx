2ae4db9393de9ca2c56a7ead71d06379958fe145
StoyanMurdzhev <smourdzhev@gmail.com>
2024-07-25 16:39:29 +0300

    Change AuthContext extension

--- vite-react-project/src/main.jsx
+++ vite-react-project/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 
 import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContext.js'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
