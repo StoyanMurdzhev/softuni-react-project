72f3a84ecfd7495afaa963fe7acda287942e6c00
StoyanMurdzhev <smourdzhev@gmail.com>
2024-07-26 16:35:30 +0300

    Create Footer.jsx

---
+++ vite-react-project/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="bg-white dark:bg-gray-900">
+                <p className="text-center text-sm text-gray-600 dark:text-gray-300">
+                    SoftUni React June 2024 Course Project
+                </p>
+        </footer>
+    )
+}
