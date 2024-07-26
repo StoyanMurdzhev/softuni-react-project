34c17d11af35a048bf6fd859d333c9e1d54aa8d9
StoyanMurdzhev <smourdzhev@gmail.com>
2024-07-26 17:45:09 +0300

    Update Header and Footer components

--- vite-react-project/src/components/footer/Footer.jsx
+++ vite-react-project/src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bg-white dark:bg-gray-900">
-                <p className="text-center text-sm text-gray-600 dark:text-gray-300">
+        <footer className="bg-black dark:bg-gray-900">
+                <p className="text-center text-sm text-white dark:text-gray-300">
                     SoftUni React June 2024 Course Project
                 </p>
         </footer>
--- vite-react-project/src/components/header/Header.jsx
+++ vite-react-project/src/components/header/Header.jsx
@@ -18,27 +18,63 @@ export default function Navbar() {
         }
     }
     return (
-        <nav>
-            <ul style={{ display: 'flex', listStyleType: 'none', padding: 0, margin: 0 }}>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/">Home</Link>
-                </li>
-                { user 
-                ? 
-                (<li style={{textDecoration: "underline"}} onClick={logoutHandler}>
-                    Logout
-                </li>)
-                :
-                (<>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                </>)
-                }
-            </ul>
+        // <nav>
+        //     <ul style={{ display: 'flex', listStyleType: 'none', padding: 0, margin: 0 }}>
+        //         <li style={{ marginRight: '10px' }}>
+        //             <Link to="/">Home</Link>
+        //         </li>
+        //         { user 
+        //         ? 
+        //         (<li style={{textDecoration: "underline"}} onClick={logoutHandler}>
+        //             Logout
+        //         </li>)
+        //         :
+        //         (<>
+        //         <li style={{ marginRight: '10px' }}>
+        //             <Link to="/login">Login</Link>
+        //         </li>
+        //         <li>
+        //             <Link to="/register">Register</Link>
+        //         </li>
+        //         </>)
+        //         }
+        //     </ul>
+        // </nav>
+        <nav className="px-6 py-4 shadow">
+            <div className="lg:items-center lg:justify-between lg:flex">
+                <div className="flex items-center justify-between">
+                    <Link to="/" className="mx-auto ">
+                        <img
+                            className="w-auto h-6 sm:h-7"
+                            src="https://firebasestorage.googleapis.com/v0/b/softuni-react-spa-project.appspot.com/o/home_2550369.png?alt=media&token=cffe9db7-9a2f-4531-9067-49852d6a0007"
+                            alt=""
+                        />
+                    </Link>
+                </div>
+                <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
+                    {user ?
+                        (<button
+                            onClick={logoutHandler}
+                            className="block px-3 py-2 text-gray-600 rounded-lg dark:text-gray-200 hover:bg-gray-100 lg:mx-2">
+                            Log out
+                        </button>)
+                        :
+                        (<>
+                            <Link to="/login"
+                                className="block px-3 py-2 text-gray-600 rounded-lg dark:text-gray-200 hover:bg-gray-100 lg:mx-2"
+                            >
+                                Login
+                            </Link>
+                            <Link to="/register"
+                                className="block px-3 py-2 text-gray-600 rounded-lg dark:text-gray-200 hover:bg-gray-100 lg:mx-2"
+                            >
+                                Register
+                            </Link>
+                        </>)
+                    }
+                </div>
+
+            </div>
         </nav>
     )
 };
