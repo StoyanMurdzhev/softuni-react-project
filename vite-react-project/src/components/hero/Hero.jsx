b38f95396b5aac4b1acde1f7fe02c69fdb8a936f
StoyanMurdzhev <smourdzhev@gmail.com>
2024-07-27 04:42:57 +0300

    Fix broken link in Hero

--- vite-react-project/src/components/hero/Hero.jsx
+++ vite-react-project/src/components/hero/Hero.jsx
@@ -12,12 +12,11 @@ export default function Hero() {
                                 Discover and share deliciours recipes!
                             </p>
                             <div className="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
-                                <a
-                                    href="#"
+                                <Link to="/recipes"
                                     className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700"
                                 >
                                     Browse Recipes
-                                </a>
+                                </Link>
                                 <Link to="/register"
                                     className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300"
                                 >
