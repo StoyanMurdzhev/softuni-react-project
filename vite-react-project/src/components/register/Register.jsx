ae4f53229801e975c27b6f52ae56a41bd9bdea7e
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-01 13:34:53 +0300

    fix email input type in Register component

--- vite-react-project/src/components/register/Register.jsx
+++ vite-react-project/src/components/register/Register.jsx
@@ -71,7 +71,7 @@ export default function Register() {
                                 </svg>
                             </span>
                             <input
-                                type="text"
+                                type="email"
                                 className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 id="email"
                                 name="email"
