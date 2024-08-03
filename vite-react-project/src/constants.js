7e831642752b1091dc20627b957b854d5edbe095
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-03 16:31:13 +0300

    Create constants.js

---
+++ vite-react-project/src/constants.js
@@ -0,0 +1,3 @@
+export const ingredientPlaceholder = "2 eggs\n200g sugar";
+
+export const mealTypes= ["Appetizer", "Salad", "Side", "Dessert", "Meat", "Seafood", "Snack", "Pasta / Risotto", "Asian cuisine", "Pizza", "Breakfast"];
