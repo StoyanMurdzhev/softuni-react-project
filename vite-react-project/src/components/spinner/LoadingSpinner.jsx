85110ee4a483a83a80c74186fab1da3f02a98071
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-04 17:48:21 +0300

    Update loading spinner placement

--- vite-react-project/src/components/spinner/LoadingSpinner.jsx
+++ vite-react-project/src/components/spinner/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import styles from "./LoadingSpinner.module.css";
 
 export default function LoadingSpinner() {
     return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center mt-8">
         <span className={styles.loader}></span>
         <p className="mt-4">Loading...</p>
     </div>
