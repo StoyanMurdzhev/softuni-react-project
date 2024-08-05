6e278ecf7615128d1613b963e4c267fb44a145e7
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-06 01:04:30 +0300

    Add dark classes to footer

--- vite-react-project/src/components/footer/Footer.jsx
+++ vite-react-project/src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-black dark:bg-gray-900 mt-auto">
+        <footer className="bg-black dark:bg-gray-900 dark:border-t-2 py-1 mt-auto">
                 <p className="text-center text-sm text-white dark:text-gray-300">
                     SoftUni React June 2024 Course Project
                 </p>
