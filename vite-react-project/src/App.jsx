f1138df20b8cd39e769c0ac474332d9ecde61373
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-06 01:20:56 +0300

    Add dark class to App.jsx

--- vite-react-project/src/App.jsx
+++ vite-react-project/src/App.jsx
@@ -18,7 +18,7 @@ import NotFound from "./components/not-found/NotFound";
 function App() {
 
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen dark:bg-gray-900">
             <Navbar />
             
             <Routes>
