1b9f04d0aaa60f93dd11c8aabb2608755d87bd10
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-07 00:06:03 +0300

    Add scroll arrow to home page

--- vite-react-project/src/components/home/Home.jsx
+++ vite-react-project/src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../hero/Hero";
 import RecentPosts from "../recent/RecentPosts";
+import ScrollArrow from "../scroll-arrow/ScrollArrow";
 
 export default function Home() {
     return (
         <>
         <Hero />
+        <ScrollArrow />
         <RecentPosts />
         </>
     );
