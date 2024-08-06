53ecc9377d628aea9c006bf948fbf15c731739fb
StoyanMurdzhev <smourdzhev@gmail.com>
2024-08-06 19:07:50 +0300

    Change logic in RecipeList to use custom hook

--- vite-react-project/src/components/recipe-list/RecipeList.jsx
+++ vite-react-project/src/components/recipe-list/RecipeList.jsx
@@ -1,91 +1,25 @@
-import { useState, useEffect } from "react";
-import { getWithPagination } from "../../services/recipeService";
 import { useSearchParams } from "react-router-dom";
+import { useRecipes } from "../../hooks/useRecipes";
 
 import RecipeCard from "../recipe-card/RecipeCard";
 import MealTypeButtons from "../buttons/MealTypeButtons";
 import LoadingSpinner from "../spinner/LoadingSpinner";
 
 export default function RecipeList() {
-    let [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
     const mealTypeParam = searchParams.get("type");
-
-    const [recipes, setRecipes] = useState([]);
-    const [lastVisible, setLastVisible] = useState(null);
-    const [isLoadingInitial, setIsLoadingInitial] = useState(true);
-    const [isLoadingNext, setIsLoadingNext] = useState(false);
-    const [hasMoreRecipes, setHasMoreRecipes] = useState(true);
-    const [error, setError] = useState(null);
     const pageSize = 6;
 
-    async function getMore(mealTypeParam) {
-        setIsLoadingNext(true);
-        setError(null);
-        try {
+    const { recipes, loadRecipes, isLoadingInitial, isLoading, hasMoreRecipes, error } = useRecipes(mealTypeParam, pageSize);
 
-            const { recipes, lastVisibleRecipe, isLastBatch } = await getWithPagination(lastVisible, pageSize, mealTypeParam);
-            const nextRecipes = recipes.map(recipe => (
-                    {
-                        ...recipe.data(),
-                        id: recipe.id
-                    }
-                )
-            );
-
-            setRecipes(previousRecipes => [...previousRecipes, ...nextRecipes]);
-
-            setLastVisible(lastVisibleRecipe);
-
-            setHasMoreRecipes(!isLastBatch);
-
-
-
-        } catch (err) {
-            setError(err);
-        } finally {
-            setIsLoadingNext(false);
-        }
-
-    }
-
-    async function handleFilter(mealType) {
+    function handleFilter (mealType) {
         if (mealType) {
             setSearchParams({ type: mealType });
         } else {
             setSearchParams({});
         }
-            setRecipes([]);
-            setLastVisible(null);
-            setHasMoreRecipes(true);
-    }
-
-    useEffect(() => {
-        (async () => {
-            setIsLoadingInitial(true);
-            setError(null);
-            
-
-            try {
-                const { recipes, lastVisibleRecipe, isLastBatch } = await getWithPagination(null, pageSize, mealTypeParam);
-                const initialRecipes = recipes.map(recipe => (
-                    {
-                        ...recipe.data(),
-                        id: recipe.id
-                    }
-                ));
-
-                setRecipes(initialRecipes);
-                setLastVisible(lastVisibleRecipe);
-                setHasMoreRecipes(!isLastBatch);
-
-            } catch (err) {
-                setError(err);
-            } finally {
-                setIsLoadingInitial(false);
-            }
-
-        })();
-    }, [mealTypeParam]);
+        loadRecipes(mealType, true);
+    };
 
     return (
         <section className="bg-white dark:bg-gray-900">
@@ -103,33 +37,36 @@ export default function RecipeList() {
                     <MealTypeButtons handleFilter={handleFilter} />
                 </div>
 
-                {error ?
-                    (
-                        <p className="max-w-lg mx-auto mt-14 text-gray-500 text-center text-2xl">
-                            {error.message}
-                        </p>)
-                    : isLoadingInitial ?
-                        <LoadingSpinner />
-                        : (
-                            <>
-                                <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3">
-                                    {recipes.map(recipe => (
-                                        <RecipeCard key={recipe.id} recipe={recipe} />
-                                    ))}
-                                </div>
-                                <div className="flex justify-center mt-8">
-                                    {isLoadingNext
-                                        ?
-                                        (<LoadingSpinner />)
-                                        :
-                                        (hasMoreRecipes && <button onClick={() => getMore(mealTypeParam)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Load more recipes</button>)
-                                    }
-                                </div>
-                            </>
-                        )}
-
+                {error ? (
+                    <p className="max-w-lg mx-auto mt-14 text-gray-500 text-center text-2xl">
+                        {error.message}
+                    </p>
+                ) : isLoadingInitial ? (
+                    <LoadingSpinner />
+                ) : (
+                    <>
+                        <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3">
+                            {recipes.map(recipe => (
+                                <RecipeCard key={recipe.id} recipe={recipe} />
+                            ))}
+                        </div>
+                        <div className="flex justify-center mt-8">
+                            {isLoading ? (
+                                <LoadingSpinner />
+                            ) : (
+                                hasMoreRecipes && (
+                                    <button
+                                        onClick={() => loadRecipes(mealTypeParam)}
+                                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                                    >
+                                        Load more recipes
+                                    </button>
+                                )
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
         </section>
-
-    )
+    );
 }
